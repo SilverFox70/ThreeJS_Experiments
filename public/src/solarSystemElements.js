b353acec24f57fa0618a05b76d846ea2159dcbda
William Brinkert <william.brinkert@akqa.com>
2023-03-21 12:21:16 -0700

    add textures and scaling details

--- public/src/solarSystemElements.js
+++ public/src/solarSystemElements.js
@@ -1,8 +1,21 @@
+// importing the function from here was causing an odd renderer.onBeforeMesh issue
 export function createPlanet(planetData) {
+  let material;
   const color = planetData.color;
+  console.log("color: ", color);
+  const geometry = new THREE.SphereGeometry(planetData.size, 32, 32);
+  if (color === 0xffa64d) {
+    material = new THREE.MeshBasicMaterial({
+      map: new THREE.TextureLoader().load(
+        'https://www.solarsystemscope.com/textures/download/2k_jupiter.jpg'
+      )
+    });
+  } else {
+    material = new THREE.MeshLambertMaterial({ color });
+  }
   const planet = new THREE.Mesh(
-    new THREE.SphereGeometry(planetData.size, 32, 32),
-    new THREE.MeshLambertMaterial({ color })
+    geometry,
+    material
   );
   planet.position.set(planetData.distance, 0, 0);
   planet.distance = planetData.distance;
